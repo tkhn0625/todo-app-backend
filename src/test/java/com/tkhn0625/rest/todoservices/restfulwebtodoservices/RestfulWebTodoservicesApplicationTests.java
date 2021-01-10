88670a24580a264203d6417da5dfde7bf3c34e4e
tkhn0625 <athleta.0315@gmail.com>
2021-01-10 11:28:58 +0900

    initialized gradle project-2

---
+++ src/test/java/com/tkhn0625/rest/todoservices/restfulwebtodoservices/RestfulWebTodoservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkhn0625.rest.todoservices.restfulwebtodoservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebTodoservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
