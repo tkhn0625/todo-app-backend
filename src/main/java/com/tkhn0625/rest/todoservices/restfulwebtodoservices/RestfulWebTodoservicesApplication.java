af4dc34676c9cea4a49c9ac5431bfd836e4709da
tkhn0625 <athleta.0315@gmail.com>
2021-01-10 11:27:58 +0900

    initialized gradle project

---
+++ src/main/java/com/tkhn0625/rest/todoservices/restfulwebtodoservices/RestfulWebTodoservicesApplication.java
@@ -0,0 +1,13 @@
+package com.tkhn0625.rest.todoservices.restfulwebtodoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebTodoservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebTodoservicesApplication.class, args);
+	}
+
+}
